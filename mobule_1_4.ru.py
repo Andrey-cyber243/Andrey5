335a37238bce3d67bda8d1532ec1e1e153997a6b
Andrey-cyber243 <Sapuntsov64@mail.ru>
2024-09-30 11:03:12 +0300

    Add files via upload

---
+++ mobule_1_4.ru.py
@@ -0,0 +1,9 @@
+my_string=input('Как ваше здоровье?:')
+name=len('Как ваше здоровье?:')
+print(name)
+print('Как ваше здоровье?:'.upper())
+print('Как ваше здоровье?:'.lower())
+print('Как ваше здоровье?:'.replace(' ','#'))
+name='Как',' ваше','здоровье?:'
+print(name[0])
+print(name[2])
